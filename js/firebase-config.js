1111603501ccd904d58972a6f3c70d1be2e257d1
DealingBackchod <dealing949@gmail.com>
2025-06-29 10:04:22 +0530

    firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "YOUR-KEY",
+  authDomain: "YOUR.firebaseapp.com",
+  projectId: "YOUR-ID",
+  storageBucket: "YOUR.appspot.com",
+  messagingSenderId: "SENDER",
+  appId: "APP-ID"
+};
+firebase.initializeApp(firebaseConfig);
